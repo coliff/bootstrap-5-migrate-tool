d0521444b26d314747ed9ab07b9459a47ffee322
coliff <christianoliff@pm.me>
2024-03-18 21:18:00 +0900

    Docs and Example Updates

--- README.md
+++ README.md
@@ -22,14 +22,25 @@ Copy your HTML templates to the `src` folder.
 Then run the following command to start the migration:
 
 ```bash
-gulp migrate
+npx gulp migrate
 ```
 
-The migrated HTML templates will be saved to the `dest` folder.
+The migrated HTML templates will overwrite the files in place by default. If you want to keep the original files, you can use the `--dest` flag to specify a destination folder. See the [Options](#options) section for more details.
 
 Read the official Bootstrap 5 migration guide here: <https://getbootstrap.com/docs/5.3/migration/>
+
+## Options
+
+Options that may be set via CLI flags.
+For example: `npx gulp migrate  --src "./src-dir" --overwrite --verbose`
+
+- `src: './src'` - string that will be passed to the gulp {@link src} function
+- `dest: ./`- string that will be passed to the gulp {@link dest} function
+- `overwrite: true` - overwrite the existing files in place. **Cannot be used with --dest flag**
+- `verbose: true`- print the path of each generated / modified file to the console
+- `defaultFileGlob: '**/*.{asp,aspx,cshtml,ejs,erb,hbs,html,htm,jsp,php,twig,vue}'` - default glob for files to search in. Default: Search all folder and files recursively
 
 ## Demo
 
 - Input: [Bootstrap 4 HTML template](https://coliff.github.io/bootstrap-5-migrate-tool/src/index.html)
-- Output: [Bootstrap 5 HTML template](https://coliff.github.io/bootstrap-5-migrate-tool/dest/index.html)
+- Output: [Bootstrap 5 HTML template](https://coliff.github.io/bootstrap-5-migrate-tool/dest/src/index.html)
--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ const DEFAULT_OPTIONS = {
   /** print the path of each generated / modified file to the console */
   verbose: true,
   /** Default glob for files to search in. Default: Search all folder and files recursively */
-  defaultFileGlob: '**/*.{asp,hbs,html,htm,php,vue}'
+  defaultFileGlob: '**/*.{asp,aspx,cshtml,ejs,erb,hbs,html,htm,jsp,php,twig,vue}'
 };
 
 async function migrate(cb) {
@@ -37,7 +37,7 @@ async function migrate(cb) {
           /<link href=["']https:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/bootstrap\/4\.\d+\.\d+\/dist\/css\/bootstrap(\.min)?\.css["'] rel=["']stylesheet["'] ?\/?>/g,
           function () {
             CDNLinksChanged++;
-            return '<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">';
+            return '<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" rel="stylesheet">';
           },
         ),
       )
@@ -45,77 +45,77 @@ async function migrate(cb) {
       .pipe(
         replace(/<link href=["']https:\/\/cdn\.jsdelivr\.net\/npm\/bootstrap@4\.\d+\.\d+\/dist\/css\/bootstrap(\.min)?\.css["'] rel=["']stylesheet["'] ?\/?>/g, function () {
           CDNLinksChanged++;
-          return '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">';
+          return '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">';
         }),
       )
       // Stackpath CSS
       .pipe(
         replace(/<link href=["']https:\/\/stackpath\.bootstrapcdn\.com\/bootstrap\/4\.\d+\.\d+\/css\/bootstrap(\.min)?\.css["'] rel=["']stylesheet["'] ?\/?>/g, function () {
           CDNLinksChanged++;
-          return '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">';
+          return '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">';
         }),
       )
       // UNPKG CSS
       .pipe(
         replace(/<link href=["']https:\/\/unpkg\.com\/bootstrap\/4\.\d+\.\d+\/css\/bootstrap(\.min)?\.css["'] rel=["']stylesheet["'] ?\/?>/g, function () {
           CDNLinksChanged++;
-          return '<link href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css">';
+          return '<link href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css">';
         }),
       )
       // CDNJS JS
       .pipe(
         replace(/<script src=["']https:\/\/cdn\.cloudflare\.com\/ajax\/libs\/bootstrap\/4\.\d+\.\d+\/dist\/js\/bootstrap(\.min)?\.js["']>/g, function () {
           CDNLinksChanged++;
-          return '<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.min.js">';
+          return '<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js">';
         }),
       )
       // JSDelivr JS
       .pipe(
         replace(/<script src=["']https:\/\/cdn\.jsdelivr\.net\/npm\/bootstrap@4\.\d+\.\d+\/dist\/js\/bootstrap(\.min)?\.js["']>/g, function () {
           CDNLinksChanged++;
-          return '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js">';
+          return '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js">';
         }),
       )
       // Stackpath JS
       .pipe(
         replace(/<script src=["']https:\/\/stackpath\.bootstrapcdn\.com\/bootstrap\/4\.\d+\.\d+\/js\/bootstrap(\.min)?\.js["']>/g, function () {
           CDNLinksChanged++;
-          return '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js">';
+          return '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js">';
         }),
       )
       // UNPKG JS
       .pipe(
         replace(/<script src=["']https:\/\/unpkg\.com\/bootstrap\/4\.\d+\.\d+\/js\/bootstrap(\.min)?\.js["']>/g, function () {
           CDNLinksChanged++;
-          return '<script src="https://unpkg.com/bootstrap@5.3.2/dist/js/bootstrap.min.js">';
+          return '<script src="https://unpkg.com/bootstrap@5.3.3/dist/js/bootstrap.min.js">';
         }),
       )
       // CDNJS Bundle JS
       .pipe(
         replace(/<script src=["']https:\/\/cdn\.cloudflare\.com\/ajax\/libs\/bootstrap\/4\.\d+\.\d+\/dist\/js\/bootstrap\.bundle(\.min)?\.js["']>/g, function () {
           CDNLinksChanged++;
-          return '<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js">';
+          return '<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js">';
         }),
       )
       // JSDelivr Bundle JS
       .pipe(
         replace(/<script src=["']https:\/\/cdn\.jsdelivr\.net\/npm\/bootstrap@4\.\d+\.\d+\/dist\/js\/bootstrap\.bundle(\.min)?\.js["']>/g, function () {
           CDNLinksChanged++;
-          return '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">';
+          return '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">';
         }),
       )
       // Stackpath Bundle JS
       .pipe(
         replace(/<script src=["']https:\/\/stackpath\.bootstrapcdn\.com\/bootstrap\/4\.\d+\.\d+\/js\/bootstrap\.bundle(\.min)?\.js["']>/g, function () {
           CDNLinksChanged++;
-          return '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">';
+          return '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">';
         }),
       )
       // UNPKG Bundle JS
       .pipe(
         replace(/<script src=["']https:\/\/unpkg\.com\/bootstrap\/4\.\d+\.\d+\/js\/bootstrap\.bundle(\.min)?\.js["']>/g, function () {
           CDNLinksChanged++;
-          return '<script src="https://unpkg.com/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">';
+          return '<script src="https://unpkg.com/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">';
         }),
       )
       .pipe(
